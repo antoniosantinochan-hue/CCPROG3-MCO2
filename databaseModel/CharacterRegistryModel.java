edd67f04ee5a2169f2360bbe668ac0a40c937207
antoniosantinochan-hue <antonio_santino_chan@dlsu.edu.ph>
2026-07-30 22:00:25 +0800

    Update CharacterRegistryModel.java

--- databaseModel/CharacterRegistryModel.java
+++ databaseModel/CharacterRegistryModel.java
@@ -2,17 +2,16 @@ package databaseModel;
 import java.util.*;
 
 public class CharacterRegistryModel {
-    public static ArrayList<Character> characterRegistry;
+    public static ArrayList<Character> characterRegistry = new ArrayList<>();
     private int characterCount = 0;
-
-    public CharacterRegistryModel(){
-        ArrayList<Character> characterRegistry =
-                new ArrayList<Character>();
-    }
 
     public void addCharacter(Character c){
         characterRegistry.add(c);
         characterCount++;
+    }
+
+    public void removeCharacter(int index){
+        characterRegistry.remove(index);
     }
 
     public ArrayList<Character> getCharacterRegistry(){
