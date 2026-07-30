b5bb6a105e5b90b9ec0e9ce18d7ba3de6298efb2
antoniosantinochan-hue <antonio_santino_chan@dlsu.edu.ph>
2026-07-30 22:03:10 +0800

    Update MainMenuController.java

--- databaseController/MainMenuController.java
+++ databaseController/MainMenuController.java
@@ -13,6 +13,61 @@ public class MainMenuController {
     }
 
     public void start(){
-        mainMenuView.generateCharacterMenu();
+        boolean isRunning;
+
+        isRunning = true;
+        while (isRunning){
+            int choice = mainMenuView.generateMainMenu();
+
+            if (choice == 1){
+                startCharacterMenu();
+            }
+
+            else if (choice == 2){
+
+            }
+
+            else if (choice == 3){
+
+            }
+
+            else if (choice == 4){
+
+            }
+
+            else if (choice == 5){
+                isRunning = false;
+                mainMenuView.sendFarewellMessage();
+            }
+        }
+    }
+
+    public void startCharacterMenu(){
+        boolean isRunning;
+
+        isRunning = true;
+        while (isRunning){
+            int choice = mainMenuView.generateCharacterMenu();
+
+            if (choice == 1){
+                characterController.addCharacter();
+            }
+
+            else if (choice == 2){
+                characterController.viewProfile();
+            }
+
+            else if (choice == 3){
+                characterController.modifyCharacter();
+            }
+
+            else if (choice == 4){
+                characterController.deleteCharacter();
+            }
+
+            else if (choice == 5){
+                isRunning = false;
+            }
+        }
     }
 }
