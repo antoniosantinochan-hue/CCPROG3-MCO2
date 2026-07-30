ec70fa4ed611e2ff87ce3ef9e8b6018833c37558
antoniosantinochan-hue <antonio_santino_chan@dlsu.edu.ph>
2026-07-30 22:02:37 +0800

    Update PirateHunterView.java

--- databaseView/PirateHunterView.java
+++ databaseView/PirateHunterView.java
@@ -1,45 +1,44 @@
 package databaseView;
 
+import databaseModel.PirateHunter;
 import java.util.Scanner;
 
 public class PirateHunterView {
-    Scanner myObj;
+    InputHandler inputHandler;
 
-    public PirateHunterView(Scanner myObj){
-        this.myObj = myObj;
+    public PirateHunterView(InputHandler inputHandler){
+        this.inputHandler = inputHandler;
     }
 
     public String promptForCombatStyle(){
         String combatStyle;
-
-        System.out.print("Enter your pirate hunter's combat style: ");
-        combatStyle = myObj.nextLine();
+        combatStyle = inputHandler.readNonEmptyString("Enter your pirate hunter's combat style: ");
 
         return combatStyle;
     }
 
     public int promptForConfirmedCaptures(){
         int confirmedCaptures;
-
-        System.out.print("Enter your pirate hunter's confirmed capture count: ");
-        confirmedCaptures = myObj.nextInt();
+        confirmedCaptures = inputHandler.readInt("Enter your pirate hunter's confirmed capture count: ");
 
         return confirmedCaptures;
     }
 
-    public void printPirateHunterDetails(int CHARACTER_ID, String name, String alias,
-                                   String origin, String status, String devilFruit,
-                                   double wallet, String combatStyle,
-                                   String confirmedCaptures){
+    public void printPirateHunterDetails(PirateHunter h){
         System.out.println("PIRATE HUNTER DETAILS");
-        System.out.printf("Character ID: %07d\n", CHARACTER_ID);
-        System.out.println("Name: " + name);
-        System.out.println("Alias: " + alias);
-        System.out.println("Origin: " + origin);
-        System.out.println("Status: " + status);
-        System.out.println("Devil Fruit Power: " + status);
-        System.out.println("Wallet: " + wallet);
-        System.out.println("Combat Style: " + combatStyle);
-        System.out.println("Confirmed Captures: " + confirmedCaptures);
+        System.out.printf("Character ID: %07d\n", h.getCHARACTER_ID());
+        System.out.println("Name: " + h.getName());
+        System.out.println("Alias: " + h.getAlias());
+        System.out.println("Origin: " + h.getOrigin());
+        System.out.println("Status: " + h.getStatus());
+        if (h.getDevilFruitPower() == null){
+            System.out.println("Devil Fruit Power: null");
+        }
+        else{
+            System.out.println("Devil Fruit Power: " + h.getDevilFruitPower().getFruitName());
+        }
+        System.out.println("Wallet: " + h.getWallet());
+        System.out.println("Combat Style: " + h.getCombatStyle());
+        System.out.println("Confirmed Captures: " + h.getConfirmedCaptures());
     }
 }
