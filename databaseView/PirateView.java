8c4cc5fc3bccf3f741e665119cacdd86ab312a58
antoniosantinochan-hue <antonio_santino_chan@dlsu.edu.ph>
2026-07-30 22:02:51 +0800

    Update PirateView.java

--- databaseView/PirateView.java
+++ databaseView/PirateView.java
@@ -1,37 +1,64 @@
 package databaseView;
 
+import databaseModel.Pirate;
 import java.util.Scanner;
 
 public class PirateView {
-    Scanner myObj;
+    InputHandler inputHandler;
 
-    public PirateView(Scanner myObj){
-        this.myObj = myObj;
+    public PirateView(InputHandler inputHandler){
+        this.inputHandler = inputHandler;
     }
 
     public double promptForBounty(){
         double bounty;
-
-        System.out.print("Enter your pirate's bounty: ");
-        bounty = myObj.nextDouble();
+        bounty = inputHandler.readDouble("Enter your pirate's bounty: ");
 
         return bounty;
     }
 
-    public void printPirateDetails(int CHARACTER_ID, String name, String alias,
-                                   String origin, String status, String devilFruit,
-                                   double wallet, double bounty, String role,
-                                   String pirateCrew){
-        System.out.println("PIRATE DETAILS");
-        System.out.printf("Character ID: %07d\n", CHARACTER_ID);
-        System.out.println("Name: " + name);
-        System.out.println("Alias: " + alias);
-        System.out.println("Origin: " + origin);
-        System.out.println("Status: " + status);
-        System.out.println("Devil Fruit Power: " + status);
-        System.out.println("Wallet: " + wallet);
-        System.out.println("Bounty: " + bounty);
-        System.out.println("Role: " + role);
-        System.out.println("Crew: " + pirateCrew);
+    public String promptForRole(){
+        String role;
+        role = inputHandler.readNonEmptyString("Enter your pirate's role: ");
+
+        return role;
     }
+
+    public void printErrorMessage(int mode, Pirate p){
+        if (mode == 1){
+            System.out.println(p.getName() + " is already a " +
+                    p.getRole() + "!");
+        }
+    }
+
+    public void printPirateDetails(Pirate p){
+        System.out.println("PIRATE DETAILS");
+        System.out.printf("Character ID: %07d\n", p.getCHARACTER_ID());
+        System.out.println("Name: " + p.getName());
+        System.out.println("Alias: " + p.getAlias());
+        System.out.println("Origin: " + p.getOrigin());
+        System.out.println("Status: " + p.getStatus());
+        if (p.getDevilFruitPower() == null){
+            System.out.println("Devil Fruit Power: null");
+        }
+        else{
+            System.out.println("Devil Fruit Power: " + p.getDevilFruitPower().getFruitName());
+        }
+        System.out.println("Wallet: " + p.getWallet());
+        System.out.println("Bounty: " + p.getBounty());
+        if (p.getRole() == null){
+            System.out.println("Role: null");
+        }
+        else{
+            System.out.println("Role: " + p.getRole());
+        }
+        if (p.getPirateCrew() == null){
+            System.out.println("Crew: null");
+        }
+        else{
+            System.out.println("Crew: " + p.getPirateCrew().getName());
+        }
+    }
+
+
 }
