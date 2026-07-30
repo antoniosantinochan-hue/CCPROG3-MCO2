c09680d8f5ea91e1e1f1a76be733c43d74f58a1a
antoniosantinochan-hue <antonio_santino_chan@dlsu.edu.ph>
2026-07-30 22:01:06 +0800

    Update CivilianView.java

--- databaseView/CivilianView.java
+++ databaseView/CivilianView.java
@@ -1,45 +1,45 @@
 package databaseView;
 
+import databaseModel.Civilian;
+
 import java.util.Scanner;
 
 public class CivilianView {
-    Scanner myObj;
+    InputHandler inputHandler;
 
-    public CivilianView(Scanner myObj){
-        this.myObj = myObj;
+    public CivilianView(InputHandler inputHandler){
+        this.inputHandler = inputHandler;
     }
 
     public String promptForProfession(){
         String profession;
-
-        System.out.print("Enter your civilian's profession: ");
-        profession = myObj.nextLine();
+        profession = inputHandler.readNonEmptyString("Enter your civilian's profession: ");
 
         return profession;
     }
 
     public String promptForResidence(){
         String residence;
-
-        System.out.print("Enter your civilian's residence: ");
-        residence = myObj.nextLine();
+        residence = inputHandler.readNonEmptyString("Enter your civilian's residence: ");
 
         return residence;
     }
 
-    public void printCivilianDetails(int CHARACTER_ID, String name, String alias,
-                                         String origin, String status, String devilFruit,
-                                         double wallet, String profession,
-                                         String residence){
+    public void printCivilianDetails(Civilian c){
         System.out.println("CIVILIAN DETAILS");
-        System.out.printf("Character ID: %07d\n", CHARACTER_ID);
-        System.out.println("Name: " + name);
-        System.out.println("Alias: " + alias);
-        System.out.println("Origin: " + origin);
-        System.out.println("Status: " + status);
-        System.out.println("Devil Fruit Power: " + status);
-        System.out.println("Wallet: " + wallet);
-        System.out.println("Profession: " + profession);
-        System.out.println("Residence: " + residence);
+        System.out.printf("Character ID: %07d\n", c.getCHARACTER_ID());
+        System.out.println("Name: " + c.getName());
+        System.out.println("Alias: " + c.getAlias());
+        System.out.println("Origin: " + c.getOrigin());
+        System.out.println("Status: " + c.getStatus());
+        if (c.getDevilFruitPower() == null){
+            System.out.println("Devil Fruit Power: null");
+        }
+        else{
+            System.out.println("Devil Fruit Power: " + c.getDevilFruitPower().getFruitName());
+        }
+        System.out.println("Wallet: " + c.getWallet());
+        System.out.println("Profession: " + c.getProfession());
+        System.out.println("Residence: " + c.getResidence());
     }
 }
